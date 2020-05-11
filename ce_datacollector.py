5bdd79c62e3d680b7bc34e8473474bbf0aa350c0
changhune <49459280+changhune@users.noreply.github.com>
2020-05-11 19:43:49 -0400

    Update ce_datacollector.py

--- ce_datacollector.py
+++ ce_datacollector.py
@@ -6,9 +6,10 @@ from flirpy.camera.lepton import Lepton
 from time import sleep
 
 ## INPUT Parameters
-E  = 44500 # Activation Energy
-kg = 2.0   # Rate Constant (Needs to be calibrated)
-q  = 6/7   # Kinetics Order (Needs to be calibrated)
+E  = 44500         # Activation Energy
+kg = 2.0           # Rate Constant (Needs to be calibrated)
+q  = 6/7           # Kinetics Order (Needs to be calibrated)
+Tref = 273.15 + 20 # Reference Temperature 
 
 dt = 15.0  # Time interval between frames (seconds) 
 t  = 0.0   # Initial physical time (hours)
@@ -21,26 +22,24 @@ while(True):
 	tempsK = getRawData(0) / 100 # Capture frame of temperatures in K
 	
 	## Convert to PSI
-	dte = np.exp(-E/8.314*(1/T1 - 1/Tref))*(dt/3600)  ## Unit: hours
-    	te += dte
+	dte = np.exp(-E/8.314*(1/tempsK - 1/Tref))*(dt/3600)  ## Unit: hours
+	te += dte
 	t  += dt
-    	P   = (kg * (1-q) * te)**(1/(1-q)) ## Unit: PSI
+	P   = (kg * (1-q) * te)**(1/(1-q))
 	
 	## Display PSI
-    	cv2.imshow('PSI View', P)
-    	cv2.waitKey(1)
-    		
+	cv2.imshow('PSI View', P)
+	cv2.waitKey(1)
 	sleep(dt) # Wait for a number of seconds between frames
 
-    if cv2.waitKey(1) & 0xFF == ord('q'): # If 'q' key pressed
-	savetxt('P.csv', data, delimiter=',')
-	print('Terminate Time = ', t/3600, 'Hours')
-		
-        break # Exit while loop
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		np.savetxt('P.csv', P, delimiter=',')
+		print('Terminate Time = ', t/3600, 'Hours')
+		break # Exit while loop
 		
 ## Get the data from an IR camera
-
 def getRawData(port):
 	camera = Lepton()
 	rawData = camera.grab(port)
 	return [rawData]
+
