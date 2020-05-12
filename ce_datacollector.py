ea8338917a4f35028fbe0925680f2291713bfa84
pjcarriere <49417278+pjcarriere@users.noreply.github.com>
2020-05-11 20:12:59 -0400

    Add files via upload

--- ce_datacollector.py
+++ ce_datacollector.py
@@ -1,4 +1,3 @@
-## Import Library
 import numpy as np
 import matplotlib.pyplot as plot
 import cv2
@@ -11,35 +10,37 @@ kg = 2.0           # Rate Constant (Needs to be calibrated)
 q  = 6/7           # Kinetics Order (Needs to be calibrated)
 Tref = 273.15 + 20 # Reference Temperature 
 
-dt = 15.0  # Time interval between frames (seconds) 
+dt = 15  # Time interval between frames (seconds) 
 t  = 0.0   # Initial physical time (hours)
 te = t     # Intiial equivalent age (hours)
 
-## Data Acquisition and Processing (degee K to PSI)
-while(True):
-	
-	## Measurement of Temperature
-	tempsK = getRawData(0) / 100 # Capture frame of temperatures in K
-	
-	## Convert to PSI
-	dte = np.exp(-E/8.314*(1/tempsK - 1/Tref))*(dt/3600)  ## Unit: hours
-	te += dte
-	t  += dt
-	P   = (kg * (1-q) * te)**(1/(1-q))
-	
-	## Display PSI
-	cv2.imshow('PSI View', P)
-	cv2.waitKey(1)
-	sleep(dt) # Wait for a number of seconds between frames
-
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		np.savetxt('P.csv', P, delimiter=',')
-		print('Terminate Time = ', t/3600, 'Hours')
-		break # Exit while loop
-		
 ## Get the data from an IR camera
 def getRawData(port):
 	camera = Lepton()
 	rawData = camera.grab(port)
 	return [rawData]
 
+## Data Acquisition and Processing (degee K to PSI)
+while(True):
+	
+	## Measurement of Temperature\
+    rawData = getRawData(0)
+    reshapedRawData = np.reshape(rawData,[120,160])
+    tempsK = reshapedRawData / 100  # Capture frame of temperatures in K
+	
+	## Convert to PSI
+    dte = np.exp(-E/8.314*(1/tempsK - 1/Tref))*(dt/3600)  ## Unit: hours
+    te += dte
+    t  += dt
+    P   = (kg * (1-q) * te)**(1/(1-q))
+	
+	## Display PSI
+    cv2.imshow('PSI View', P)
+    cv2.waitKey(1)
+    sleep(dt) # Wait for a number of seconds between frames
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        np.savetxt('P.csv', P, delimiter=',')
+        print('Terminate Time = ', t/3600, 'Hours')
+        break # Exit while loop
+		
